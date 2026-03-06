f8736a07c84844960e0de50e2b76800d52a6c0a7
Fedor Kaputsky <fedor.kaputsky@sap.com>
2026-03-06 23:35:26 +0300

    feat: add the solution for the merge.js task

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -1,8 +1,53 @@
+import { readdir, readFile, writeFile } from "fs/promises";
+import { extname, join } from "path";
+import { cwd } from "process";
+
+const workspaceFolderName = "workspace";
+const partsFolderName = "parts";
+const rootPath = cwd();
+const fullPath = join(rootPath, workspaceFolderName, partsFolderName);
+const targetFileName = "merged.txt";
+const targetPath = join(rootPath, workspaceFolderName, targetFileName);
+const defaultFileExt = ".txt";
+
+
+// accepts comma-separated names like --files name1,name2,name3
 const merge = async () => {
-  // Write your code here
-  // Default: read all .txt files from workspace/parts in alphabetical order
-  // Optional: support --files filename1,filename2,... to merge specific files in provided order
-  // Concatenate content and write to workspace/merged.txt
+  try {
+    let names = [];
+    let resultParts = [];
+
+    const [flag, filenames] = process.argv.slice(2);
+
+    if (flag === "--files"
+      && filenames?.split(",").length) {
+      names = filenames.split(",");
+    }
+
+    if (names.length) {
+      for (const name of names) {
+        const content = await readFile(join(fullPath, name + defaultFileExt), "utf-8");
+        resultParts.push(content);
+      }
+    }
+    else {
+      const dirData = await readdir(fullPath);
+      const txtFiles = dirData.filter(name => extname(name) === defaultFileExt);
+      if (!txtFiles.length) {
+        throw new Error();
+      } else {
+        for (const name of txtFiles) {
+          const content = await readFile(join(fullPath, name), "utf-8");
+          resultParts.push(content);
+        }
+        resultParts.sort();
+      }
+    }
+    await writeFile(targetPath, resultParts.join(""), "utf-8");
+
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await merge();
