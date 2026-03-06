dd98a97b1c282591b406400a685df110fb4674b8
Fedor Kaputsky <fedor.kaputsky@sap.com>
2026-03-06 22:35:31 +0300

    feat: add the code solution for findByExt.js

--- src/fs/findByExt.js
+++ src/fs/findByExt.js
@@ -1,7 +1,44 @@
+import { readdir, readFile, stat, writeFile } from "fs/promises";
+import { extname, join, relative, sep } from "path";
+import { cwd } from "process";
+
+const folderName = "workspace";
+const rootPath = cwd();
+const fullPath = join(rootPath, folderName);
+const normalizePath = value => value.split(sep).join("/");
+
+
 const findByExt = async () => {
-  // Write your code here
-  // Recursively find all files with specific extension
-  // Parse --ext CLI argument (default: .txt)
+  let extension = ".txt";
+  const [flag, ext] = process.argv.slice(2);
+  if (flag === "--ext" && ext?.length) {
+    extension = ext.startsWith(".") ? ext : `.${ext}`;
+  }
+
+  try {
+    const dirData = await readdir(fullPath, { withFileTypes: true, recursive: true });
+    const result = [];
+
+    for (const dirent of dirData) {
+
+      const { name, parentPath } = dirent;
+      const dirPath = join(parentPath, name);
+      const path = normalizePath(relative(fullPath, dirPath));
+
+      if (dirent.isFile()) {
+        const extensionName = extname(path);
+        if (extensionName === extension) {
+          result.push(path);
+        }
+      }
+    }
+
+    result.sort();
+    result.forEach(item => console.log(item));
+
+  } catch (error) {
+    console.log("FS operation failed");
+  }
 };
 
 await findByExt();
