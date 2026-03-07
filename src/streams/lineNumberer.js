b5de6e42daf731f9f04c0a935635f3a686a99a49
Fedor Kaputsky <fedor.kaputsky@sap.com>
2026-03-07 22:29:23 +0300

    feat: add the solution for the lineNumberer.js task

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,33 @@
+import { Transform } from 'stream';
+
+// To get the strict result (`1 | hello\n2 | world`) don't change anything
+const separator = '\\n';
+
+// to get every line on next line you can use the separator below.
+// P.S. I just don't understand if the requirement is written correctly :)
+// const separator = '\n';
+
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+  let lineNumber = 1;
+
+  const transform = new Transform({
+    transform(chunk, _encoding, callback) {
+      const data = chunk.toString().replace(/\\n/g, '\n');
+      const lines = data.split('\n');
+      lines.pop();
+
+      const output = lines
+        .map(line => `${lineNumber++} | ${line}`)
+        .join(separator);
+
+      if (output) {
+        this.push(output + '\n');
+      }
+      callback();
+    }
+  });
+
+  process.stdin.pipe(transform).pipe(process.stdout);
 };
 
 lineNumberer();
